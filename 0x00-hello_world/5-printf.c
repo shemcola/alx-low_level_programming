0cf55aaa17ca6c5aad9ce75b4ba356ffcfa6925b
shemcola <shem.b.cola@gmail.com>
2023-03-16 04:29:57 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-* main - A program that print a line using printf fucntion
- * Retur: 0 (Success)
+ * main - A program that print a line using printf fucntion
+ * Return: 0 (Success)
  */
 int main(void)
 {
