76387a6a6bee657a0986c0c7ad7c061f4ea438d4
shemcola <shem.b.cola@gmail.com>
2023-03-17 17:34:31 -0700

    hgha

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,13 +4,13 @@
  *
  * Return: always 0
  */
+
 int main(void)
 {
 	char i;
 
-	for (i = 0; i < 10; i++)
-		putchar(i + '0');
+	for (i = 'z'; i >= 'a'; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
-
 }
