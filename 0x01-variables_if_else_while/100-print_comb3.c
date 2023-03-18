c64e7ec80d0c0d090aa2df191acfc4c5ee2167b5
shemcola <shem.b.cola@gmail.com>
2023-03-17 19:37:03 -0700

    kjjk

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 	int j;
 	int i;
 
-	for (i == 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 1; j < 10; j++)
 		{
 			if (i < j && i != j)
 			{
 				putchar(i + '0');
-				putchar(i + '0');
-				puthar(',');
+				putchar(j + '0');
+				putchar(',');
 				putchar(' ');
 			}
 		}
