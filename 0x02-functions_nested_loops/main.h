ba96b1caad948e0d84ab27b5e663f2ca79547472
shemcola <shem.b.cola@gmail.com>
2023-03-21 03:27:24 -0700

    first task

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void peint_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_beuer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
