e46747db902ff61dfb04664712a6997d9213d194
shemcola <shem.b.cola@gmail.com>
2023-03-21 04:42:34 -0700

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print a_alphabets
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+
+	char c;
+	for (c = 'a'; c <= '2'; c++)
+	{
+	_putchar(c);
+	}
+	{
+	_putchar('\n');
+	}
+}	
