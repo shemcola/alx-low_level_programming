8f88932daa0f2b89aabd74470b0e81605b402fa1
shemcola <shem.b.cola@gmail.com>
2023-03-21 08:17:16 -0700

    print in lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ * @c: The character to  e chekced
+ * Return: 1 for lowercase haracters or 0 for else
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	return (0);
+}
