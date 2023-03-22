28657a734680b64be238cdd2e46fe6bc3a1c7e21
shemcola <shem.b.cola@gmail.com>
2023-03-22 00:35:56 -0700

    tas12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,7 +23,9 @@ void print_times_table(int n)
 	prod = num * mult;
 	if (prod <= 99)
 	_putchar(' ');
-	if (prod >= 100)
+	if (prod <= 9)
+	_putchar(' ');
+	if (prod >+ 100)
 	{
 	_putchar((prod / 100) + '0');
 	_putchar(((prod / 10)) % 10 + '0');
